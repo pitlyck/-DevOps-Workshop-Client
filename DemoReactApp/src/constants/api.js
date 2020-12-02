b27c7cc130090b5ac0ed1f960283b1e3306991b0
michalp <michalp@kentico.com>
2020-12-02 08:22:20 +0100

    DEVOPS-123 API URI

--- DemoReactApp/src/constants/api.js
+++ DemoReactApp/src/constants/api.js
@@ -1,4 +1,4 @@
-const API_URI = 'https://pv247messaging.azurewebsites.net/api/v2';
+const API_URI = 'https://michalp-workshopapi.azurewebsites.net/api/v2';
 const API_APP_ID = 'f1a58775-114f-46c0-ab5b-9712dac5a285';
 
 export const API_AUTH_URI = `${API_URI}/auth`;
