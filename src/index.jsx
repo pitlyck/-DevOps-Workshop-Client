42e5faf7353158f77a8cc36f34a4be741b602f38
Vilém Jeniš <vilemj@kentico.com>
2020-12-07 11:37:19 +0100

    Add configurable BE URL.

--- src/index.jsx
+++ src/index.jsx
@@ -9,5 +9,15 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import { AppWrapper } from './shared/components/AppWrapper.tsx';
+import { setConfig } from './shared/config.ts';
 
-ReactDOM.render(<AppWrapper/>, document.getElementById('app-root'));
+const main = async () => {
+  const resp = await fetch('/env/backendurl');
+  const url = await resp.text();
+  console.log(url);
+  setConfig({ url });
+
+  ReactDOM.render(<AppWrapper/>, document.getElementById('app-root'));
+};
+
+main();
